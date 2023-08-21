00fba63589cfb599ac14992c280abf7657ee03ed
Renato Albuquerque <renatomalb83@gmail.com>
2023-08-21 00:26:03 -0300

    updated js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-const url = "https://jsonplaceholder.typicode.com/posts"
+const url = "https://jsonplaceholder.typicode.com/posts";
 
-const loadingElement = document.querySelector("#loading")
-const postsContainer = document.querySelector("#posts-container")
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
+
+const commentForm = document.querySelector("#comment-form");
+const emailInput = document.querySelector("#email");
+const bodyInput = document.querySelector("#body");  
+
+// load post
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
 
 // get all posts
 
@@ -31,4 +44,81 @@ async function getAllPosts() {
     })
 }
 
-getAllPosts();
+// get individual post
+
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`)
+    ]);
+
+    const dataPost = await responsePost.json();
+    const dataComments = await responseComments.json();
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
+
+    dataComments.map((comment) => {
+        createComment(comment);
+    })
+}
+
+function createComment(comment) {
+    const div = document.createElement("div");
+    const email = document.createElement("h3");
+    const commentBody = document.createElement("p");
+
+    email.innerText = comment.email;
+    commentBody.innerText = comment.body;
+
+    div.appendChild(email);
+    div.appendChild(commentBody);
+
+    commentsContainer.appendChild(div)
+}
+
+// Insert a comment
+async function postComment(comment) {
+    const response = await fetch(url, {
+      method: "POST",
+      body: comment,
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  
+    const data = await response.json();
+  
+    createComment(data);
+}
+
+if(!postId) {
+    getAllPosts();
+} else {
+    getPost(postId);
+}
+
+// add event to comment form
+
+commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let comment = {
+        email: emailInput.value,
+        body: bodyInput.value,
+    };
+
+    comment = JSON.stringify(comment);
+
+    postComment(comment);
+});
+
